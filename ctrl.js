31f23e9aad47f0596ddab4335908d98bb850b317
井上 敬介 <kskinoue@gmail.com>
2016-08-15 19:56:05 +0900

    カラーマップ設定変更用のインターフェースを追加

---
+++ ctrl.js
@@ -0,0 +1,16 @@
+function openSetting()
+{
+	$("#ctrlbtn").hide();
+	$("#ctrlpane").show();
+}
+
+function applySetting()
+{
+
+}
+
+function closeSetting()
+{
+	$("#ctrlpane").hide();
+	$("#ctrlbtn").show();
+}
